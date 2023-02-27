84300cef0248a17a360bc02ea9e995870a83bd8c
Evgeniya <kapatoy@yandex.ru>
2023-02-27 18:35:05 +0300

    HW001_27.02

---
+++ HW001/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число: ");
+
+int i = int.Parse(Console.ReadLine());
+
+int firstNum = i / 10000;
+int secondNum = i / 1000 % 10;
+int fourNum = i / 10 % 10;
+int fiveNum = i % 10;
+
+if (firstNum == fiveNum && secondNum == fourNum)
+{
+    Console.WriteLine($"Число {i} является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
+
+
+
+
